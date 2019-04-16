e52dfc03890c58c70f1aa9fd96f09bc8f0393ac0
Eason <115010263@link.cuhk.edu.cn>
2019-04-15 20:34:01 -0500

    Delete_song_Completed

--- app/src/main/java/com/csci5115/activities/MySongRecyclerViewAdapter.java
+++ app/src/main/java/com/csci5115/activities/MySongRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.csci5115.activities.SongFragment.OnListFragmentInteractionListener;
@@ -22,6 +23,8 @@ public class MySongRecyclerViewAdapter extends RecyclerView.Adapter<MySongRecycl
 
     private final List<Song> mValues;
     private final OnListFragmentInteractionListener mListener;
+
+    public ImageButton mBtSongRemove;
 
     public MySongRecyclerViewAdapter(List<Song> items, OnListFragmentInteractionListener listener) {
         mValues = items;
@@ -66,18 +69,36 @@ public class MySongRecyclerViewAdapter extends RecyclerView.Adapter<MySongRecycl
         public final TextView mTimeView;
         public final TextView mArtistView;
         public Song mItem;
+
         public ViewHolder(View view) {
+
             super(view);
             mView = view;
             mIdView = (TextView) view.findViewById(R.id.item_number);
             mContentView = (TextView) view.findViewById(R.id.name);
             mTimeView = (TextView) view.findViewById(R.id.Time);
             mArtistView = (TextView) view.findViewById(R.id.Singer_name);
+            mBtSongRemove = (ImageButton) view.findViewById(R.id.delete_song);
+
+            mBtSongRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    removeAt(getPosition());
+                }
+            });
+
         }
+
 
         @Override
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
         }
+    }
+
+    public void removeAt(int position) {
+        mValues.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mValues.size());
     }
 }
